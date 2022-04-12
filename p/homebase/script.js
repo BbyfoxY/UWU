97a99a09f57e7de88ef07414a6f78c9c1c55a528
BbyfoxY <wesley.james2019@gmail.com>
2022-04-12 15:39:45 +0000

    commit

--- p/homebase/script.js
+++ p/homebase/script.js
@@ -1,6 +1,6 @@
-import { credits } from '/script.js';
-import { scrap } from '/script.js';
-import { clickPow } from '/script.js';
+import { credits } from '/UWU/script.js';
+import { scrap } from '/UWU/script.js';
+import { clickPow } from '/UWU/script.js';
 
 var c = document.getElementById('credits');
 var s = document.getElementById('scrap');
